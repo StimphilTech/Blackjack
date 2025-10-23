20c85d8433decbf6e57477238644766cb3fae452
Monalisa Stimphil <stimphiltechnologist@gmail.com>
2025-10-23 10:22:55 -0400

    arraylist main commit

--- src/main/java/com/pluralsight/Blackjack.java
+++ src/main/java/com/pluralsight/Blackjack.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Blackjack {
+    private static final ArrayList<Card> Cards = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+
+    }
 }
