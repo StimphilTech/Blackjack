f36904915279dc5e0ca6ae320c419bbfcd2388db
Monalisa Stimphil <stimphiltechnologist@gmail.com>
2025-10-23 11:33:59 -0400

    desk shuffle commit

--- src/main/java/com/pluralsight/Blackjack.java
+++ src/main/java/com/pluralsight/Blackjack.java
@@ -1,11 +1,31 @@
 package com.pluralsight;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class Blackjack {
-    private static final ArrayList<Card> Cards = new ArrayList<>();
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("What is your name?");
+            String player1 = scanner.nextLine();
+            System.out.println("What is your name?");
+            String player2 = scanner.nextLine();
+            System.out.println(player1 + " VS "+ player2);
+
+            Deck deck = new Deck();
+
+        deck.shuffle();
+
+        //collections.deck: I knew I needed to shuffle the deck and then assign the players.
+
+
+
+
+
+
 
 
     }
