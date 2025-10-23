22640cfcf4bb52709447407ae33e49cfa931a200
Monalisa Stimphil <stimphiltechnologist@gmail.com>
2025-10-23 11:54:04 -0400

    handValue commit

--- src/main/java/com/pluralsight/Blackjack.java
+++ src/main/java/com/pluralsight/Blackjack.java
@@ -9,24 +9,28 @@ public class Blackjack {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-            System.out.println("What is your name?");
-            String player1 = scanner.nextLine();
-            System.out.println("What is your name?");
-            String player2 = scanner.nextLine();
-            System.out.println(player1 + " VS "+ player2);
+        System.out.println("What is your name?");
+        String player1 = scanner.nextLine();
+        System.out.println("What is your name?");
+        String player2 = scanner.nextLine();
+        System.out.println(player1 + " VS " + player2);
 
-            Deck deck = new Deck();
+        Deck deck = new Deck();
+        Hand hand1 = new Hand();
 
-        deck.shuffle();
-
-        //collections.deck: I knew I needed to shuffle the deck and then assign the players.
-
-
-
-
-
-
-
+        for (int I = 0; I < 5; I++) {
+            //get a card from the deck
+            Card card = deck.deal();
+            //deal that card to the hand
+            hand1.deal(card);
+        }
+        int handValue = hand1.getValue();
+        System.out.println( "\nThis hand is worth " + handValue);
 
     }
+
+    //collections.deck: I knew I needed to shuffle the deck and then assign the players.
+
+
 }
+
